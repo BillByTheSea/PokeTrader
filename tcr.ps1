278c8861b8a6d6e03ef7c4d420bda16bc45f8aa1
BillByTheSea <jon@ypsilantipiano.com>
2024-06-21 17:13:11 -0400

    Add a tcr script to play with the test-commit-revert idea

---
+++ tcr.ps1
@@ -0,0 +1,12 @@
+# Run Python unittest
+python -m unittest
+
+# Check the exit code of the last command
+if ($LASTEXITCODE -eq 0) {
+    # Test passed, stage changes and commit
+    git add .
+    git commit -m "Working"
+} else {
+    # Test failed, reset to HEAD
+    git reset --hard HEAD
+}
